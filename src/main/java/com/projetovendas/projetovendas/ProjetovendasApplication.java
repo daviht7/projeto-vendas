fe78b6e1cdf48ba89d17604b14b09418f2edc347
Davi Holanda <daviht7@gmail.com>
2021-11-06 17:16:54 -0300

    commit projeto inicial

---
+++ src/main/java/com/projetovendas/projetovendas/ProjetovendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetovendas.projetovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetovendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetovendas/projetovendas/ProjetovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetovendas.projetovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
